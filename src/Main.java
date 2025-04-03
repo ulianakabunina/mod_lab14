48fdf217f7284996fe1fe0e17a643ecbed17111f
uliana <kabunina@ulia>
2025-04-03 08:08:23 +0300

    kzkzkzаа2

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,15 @@ import  java.util.regex.Matcher;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner in1 = new Scanner(System.in);
+        Scanner in2 = new Scanner(System.in);
         System.out.print("Введите количество айпишников: ");
-        int n = input.nextInt();
-        input.nextLine();
+        int n = in1.nextInt();
 
         String[] adresses = new String[n];
         System.out.println("Введите айпишники: ");
         for (int i = 0; i < n; i++) {
-            adresses[i] = input.nextLine();
+            adresses[i] = in2.nextLine();
         }
 
         String reg = "^((25[0-5]|2[0-4][0-9]|1\\d\\d|[1-9]?\\d)\\.){3}(25[0-5]|2[0-4][0-9]|1\\d\\d|[1-9]?\\d)$";
